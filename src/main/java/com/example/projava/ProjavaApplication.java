68e643463fecdfede92367a23f47955a1374ca43
joaopaulobrasilio <joaopaulobrasilio947@gmail.com>
2023-04-18 14:42:00 -0300

    iniciando projeto

---
+++ src/main/java/com/example/projava/ProjavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.projava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projava/ProjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
